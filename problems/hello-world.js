06863792f10f28eeaeaae75af2d94be326c76809
Farrah Isabelle Rios <isabellefarrah80@gmail.com>
2020-12-12 12:43:47 -0500

    complete lab

--- problems/hello-world.js
+++ problems/hello-world.js
@@ -4,7 +4,7 @@
  * @returns undefined
  */
 function helloWorld() {
-
+console.log("hello world")
 }
 
 
@@ -14,8 +14,8 @@ function helloWorld() {
  * @param {string} name - the name to be added to the console log 
  * @returns undefined
  */
-function helloName() {
-
+function helloName(name) {
+console.log(`hello ${name}`)
 }
 
 /**
@@ -23,8 +23,8 @@ function helloName() {
  * 
  * @returns {string} - the string "hello world"
  */
-function returnHelloWorld() {
-
+function returnHelloWorld(string="hello world") {
+return string
 }
 
 /**
@@ -34,8 +34,8 @@ function returnHelloWorld() {
  * @param {string} name - the name to added to the return string
  * @returns {string} A greeting in the form of "Hello ${name}!"
  */
-function returnHelloName() {
-
+function returnHelloName(name) {
+return `Hello ${name}!`
 }
 
 module.exports = {
